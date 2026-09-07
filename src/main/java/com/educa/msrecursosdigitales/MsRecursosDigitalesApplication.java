dde598d5cbf3bb9d716ed2a5e8ef7dfbb7685d09
mathiasAla <150154710+mathiasAla@users.noreply.github.com>
2026-09-07 17:57:27 -0300

    Update MsRecursosDigitalesApplication.java

--- src/main/java/com/educa/msrecursosdigitales/MsRecursosDigitalesApplication.java
+++ src/main/java/com/educa/msrecursosdigitales/MsRecursosDigitalesApplication.java
@@ -1,1 +1,11 @@
+package com.educa.msrecursosdigitales;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRecursosDigitalesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsRecursosDigitalesApplication.class, args);
+    }
+}
